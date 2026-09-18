f94c9a756e428911d9e256a09d52c855a9d472ae
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-09-18 12:11:56 -0500

    feat: TOC chapter and subsections

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -26,21 +27,31 @@ func ExampleScrape() {
 		log.Fatal(err)
 	}
 
+	TOC(doc, true)
 	// article, err := doc.Find("#preamble, #toc ul li").Html()
 	// if err != nil {
 	// 	log.Fatal(err)
-	// }
 	//
+}
 
+func TOC(doc *goquery.Document, showSubsections bool) {
 	doc.Find("#toc a").Each(func(i int, link *goquery.Selection) {
-		title := normalizeWhitespace(link.Text())
+		title := normalizeWhitespaceAndQuotes(link.Text())
 		// href, _ := link.Attr("href")
 
-		fmt.Printf("[%02d] title=%q \n", i, title)
+		if match, _ := regexp.MatchString("[0-9]+.[0-9]+", title); match {
+			if !showSubsections {
+				return
+			}
+			fmt.Printf("	%s \n", title)
+		} else {
+			fmt.Printf("%s \n", title)
+		}
+		// fmt.Printf("[%02d] title=%q \n", i, title)
 	})
 }
 
-func normalizeWhitespace(value string) string {
+func normalizeWhitespaceAndQuotes(value string) string {
 	return strings.Join(strings.Fields(value), " ")
 }
 
