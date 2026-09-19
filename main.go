05413dd31ad18e8e52d061164ebf2b17ccbdf246
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-09-19 15:35:28 -0500

    feat: use fetcher instead

--- main.go
+++ main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"net/http"
+	"sep2epub/internal/fetcher"
 	"sep2epub/internal/sep"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
-	res, err := http.Get("https://plato.stanford.edu/entries/consciousness/")
+func debugPrint(chapters []sep.Chapter, chapterNum int) {
+	fmt.Println()
+	chapter := chapters[chapterNum+1]
+	fmt.Println(chapter.Title)
+	fmt.Println()
+	for _, p := range chapter.Paragraphs {
+		fmt.Println(p)
+		fmt.Println()
+	}
+}
+
+func main() {
+	client := fetcher.New()
+	body, err := client.Fetch(context.Background(), "https://plato.stanford.edu/entries/consciousness/")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer res.Body.Close()
+	defer body.Close()
 
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	sep.TableOfContents(doc, true)
 	sep.Content(doc)
-}
-
-func main() {
-	ExampleScrape()
 }
