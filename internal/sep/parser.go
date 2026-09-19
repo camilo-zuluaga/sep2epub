082ed5eebd85c85138c899b50153c4a8859159cc
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-09-19 15:19:05 -0500

    feat: normalize whitespace

--- internal/sep/parser.go
+++ internal/sep/parser.go
@@ -23,9 +23,9 @@ func Content(doc *goquery.Document) {
 	var chapters []Chapter
 	var currChapter *Chapter
 
-	doc.Find(MainTextTag).ChildrenFiltered("h2, p").Each(func(_ int, element *goquery.Selection) {
+	doc.Find(MainTextTag).ChildrenFiltered("h2, p, blockquote").Each(func(_ int, element *goquery.Selection) {
 		tagName := goquery.NodeName(element)
-		text := strings.TrimSpace(element.Text())
+		text := normalizeWhitespace(strings.TrimSpace(element.Text()))
 
 		if text == "" {
 			return
@@ -40,6 +40,8 @@ func Content(doc *goquery.Document) {
 			currChapter = &chapters[len(chapters)-1]
 		case "p":
 			currChapter.Paragraphs = append(currChapter.Paragraphs, text)
+		case "blockquote":
+			currChapter.Paragraphs = append(currChapter.Paragraphs, fmt.Sprintf("%q", text))
 		}
 	})
 	fmt.Println()
@@ -57,7 +59,7 @@ func TableOfContents(doc *goquery.Document, showSubsections bool) {
 	fmt.Printf("%s\n\n", main_title)
 
 	doc.Find(TableOfContentsTag).Each(func(i int, link *goquery.Selection) {
-		title := normalizeWhitespaceAndQuotes(link.Text())
+		title := normalizeWhitespace(link.Text())
 		if match, _ := regexp.MatchString("[0-9]+.[0-9]+", title); match {
 			if !showSubsections {
 				return
@@ -69,6 +71,6 @@ func TableOfContents(doc *goquery.Document, showSubsections bool) {
 	})
 }
 
-func normalizeWhitespaceAndQuotes(value string) string {
+func normalizeWhitespace(value string) string {
 	return strings.Join(strings.Fields(value), " ")
 }
