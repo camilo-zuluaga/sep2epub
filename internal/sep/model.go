d2640f936b20231e9ca83277339a65bf800d2bd9
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-09-22 09:24:05 -0500

    feat: add book struct for epub gen

--- internal/sep/model.go
+++ internal/sep/model.go
@@ -1,5 +1,16 @@
 package sep
 
+type HTMLTag int
+
+const (
+	Unknown = iota
+	Paragraph
+	Heading2
+	Heading3
+	BlockQuote
+	List
+)
+
 type Chapter struct {
 	Title      string
 	Paragraphs []Block
@@ -13,4 +24,10 @@ type Block struct {
 	Type    HTMLTag
 	Content string
 	List    []string
+}
+
+// usage for epub generation
+type Book struct {
+	Title    string
+	Chapters []Chapter
 }
